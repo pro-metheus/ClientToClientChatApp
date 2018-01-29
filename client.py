daa003fa0f3d4d8297db992c1c298a9aa70db70d
ProMetheus <ajayajith19@gmail.com>
2018-01-29 22:05:46 +0530

    Add files via upload

---
+++ client.py
@@ -0,0 +1,39 @@
+
+from socket import socket, AF_INET, SOCK_STREAM
+
+from pickle import dumps,loads
+
+
+
+def client_handler():
+    client = socket(AF_INET, SOCK_STREAM)
+    client.connect(('127.0.0.1', 1234))
+    handshake = client.recv(1024).decode()
+    name = input(handshake)
+    client.send(name.encode())
+    while True:
+        code = input('Enter 0 for seeing online list\n1 for sending message\n2 for viewing messages\n~')
+        if code == '0':
+            print('***refreshing***')
+            message = ['0','whos online?']
+            client.send(dumps(message))
+            print('refresing...')
+            online = loads(client.recv(1024))
+            for person in online:
+                print('(.)'+person)
+            print('\n')
+        if code == '1':
+            message = input('enter username//message :')
+            uname,message=message.split('//')
+            message = dumps([1, uname, message])
+            client.send(message)
+            error = client.recv(1024).decode()
+            if error == '1':
+                print('**message sent**')
+        if code == '2':
+            new = client.recv(1024).decode()
+            print(new)
+
+
+if __name__ =='__main__':
+    client_handler()
---
+++ server.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python3.6
+
+
+'''
+simple client-to-client messenger server written in python
+
+'''
+
+from socket import socket, AF_INET, SOCK_STREAM
+from threading import Thread
+from pickle import dumps,loads
+threads = []
+connections = {}
+
+
+
+def server_handler(client):
+    client.send('enter your name'.encode())
+    name = client.recv(1024).decode()
+    connections[name] = client
+    #updated dictionary
+    print('*** user {0} has entered the application***'.format(name))
+    while True:
+        command = loads(client.recv(1024))
+        code = str(command[0])
+        print('code {0} recieved from {1}'.format(code, name))
+        if code == '0': #client wants to know whos online
+            online = list(connections.keys())
+            client.send(dumps(online))
+        elif code == '1': #client wants to send a message
+            print('***entered sender***')
+            print('sending {0} to {1}'.format(command[2],command[1]))
+            text = name+' : '+command[2]
+            message = text.encode()
+            reciever = connections[command[1]]
+            reciever.send(message)
+            client.send('1'.encode())
+            
+        
+        
+    
+
+
+if __name__ =='__main__':
+    server = socket(AF_INET, SOCK_STREAM)
+    server.bind(('127.0.0.1', 1234))
+    server.listen(5)
+    while True:
+        client, addr = server.accept()
+        t = Thread(target=server_handler, args=(client,))
+        threads.append(t)
+        t.start()
